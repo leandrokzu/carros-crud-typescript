f3a3c27c78a3e3ad994fc56f4185db16b0ba5e6c
Leandro Augusto <leandroaugusto470@gmail.com>
2023-05-31 18:35:45 -0300

    configuração banco de dados, postgres

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
-import express from "express";
+import 'dotenv/config';
+import express from 'express';
 
 const app = express();
 
 app.use(express.json());
 
-app.listen("3000", () => {
-  console.log("Servidor inicializado na porta 3000");
+app.listen('3000', () => {
+  console.log('Servidor inicializado na porta 3000');
 });
