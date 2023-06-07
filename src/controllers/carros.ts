57dfd8172de6f250daf89f61e3acbea7a6f5f724
Leandro Augusto <leandroaugusto470@gmail.com>
2023-06-07 18:54:36 -0300

    rotas de lista e busca carros

--- src/controllers/carros.ts
+++ src/controllers/carros.ts
@@ -1,11 +1,35 @@
 import { Request, Response } from 'express';
+import { knex } from '../database/conexao';
+import { Carro } from '../tipos';
 
-export const listarCarros = (req: Request, res: Response) => {};
+export const listarCarros = async (_: Request, res: Response) => {
+  try {
+    const carros = await knex('carros');
+    return res.json(carros);
+  } catch {
+    return res.status(500).json({ message: 'Erro interno do servidor' });
+  }
+};
 
-export const detalharCarros = (req: Request, res: Response) => {};
+export const detalharCarros = async (req: Request, res: Response) => {
+  const { id } = req.params;
 
-export const cadastrarCarros = (req: Request, res: Response) => {};
+  try {
+    const carro = await knex<Carro>('carros')
+      .where({ id: Number(id) })
+      .first();
 
-export const atualizarCarros = (req: Request, res: Response) => {};
+    if (!carro) {
+      return res.status(404).json({ mensagem: 'Carro não encontrado' });
+    }
+    return res.json(carro);
+  } catch {
+    return res.status(500).json({ message: 'Erro interno do servidor' });
+  }
+};
 
-export const excluirCarros = (req: Request, res: Response) => {};
+export const cadastrarCarros = async (req: Request, res: Response) => {};
+
+export const atualizarCarros = async (req: Request, res: Response) => {};
+
+export const excluirCarros = async (req: Request, res: Response) => {};
